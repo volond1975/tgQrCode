df7053106fe7f941d07afd5636be8eaa9d7bf34a
volond1975 <volond@gmail.com>
2024-09-05 12:05:43 +0000

    sendTicket ()

--- js/qrscanner.js
+++ js/qrscanner.js
@@ -1,3 +1,10 @@
+var scaner = {
+	//date: document.querySelector('input[type="date"]'),
+	//time: document.querySelector('input[type="time"]'),
+	text: document.querySelector('#scanResult')
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const scanButton = document.getElementById('scanButton');
     const scanResult = document.getElementById('scanResult');
@@ -9,6 +16,8 @@ document.addEventListener('DOMContentLoaded', function() {
             }, function(result) {
                 if (result) {
                     scanResult.textContent = "Scanned result: " + result;
+                 
+
                 } else {
                     scanResult.textContent = "Scanning cancelled or failed";
                 }
@@ -18,3 +27,24 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+function init () {
+	//setupOptions()
+
+	Telegram.WebApp.ready()
+	Telegram.WebApp.MainButton
+		.setText('Ok')
+		.onClick(sendTicket)
+}
+
+function sendTicket () {
+	var timestamp = pickers.date.value
+		? new Date(pickers.date.value)
+		: new Date()
+
+	var [ h, m ] = pickers.time.value.split(':')
+	timestamp.setHours(h || 0, m || 0)
+
+	var data = scaner.text
+	Telegram.WebApp.sendData(data)
+}
